2fdce3c46432af4a4ad2ee46e28ad6fdf8811900
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2020-09-19 10:28:55 +0530

    selenium with sauce labs integration

---
+++ src/test/java/com/mytests/BaseTest.java
@@ -0,0 +1,64 @@
+package com.mytests;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	WebDriver driver;
+
+	@Parameters({"browser", "platform", "version"})
+	@BeforeMethod
+	public void setUp(String browserName, String platformName, String versionName,  Method name) {
+
+		System.out.println("browser name is : " + browserName);
+		String methodName = name.getName();
+
+		MutableCapabilities sauceOpts = new MutableCapabilities();
+		sauceOpts.setCapability("name", methodName);
+		sauceOpts.setCapability("build", "Java-W3C-Examples");
+		sauceOpts.setCapability("seleniumVersion", "3.141.59");
+		sauceOpts.setCapability("username", "naveenanimation20");
+		sauceOpts.setCapability("accessKey", "f418309b-b7b4-424d-825c-0d4a78b90bb5");
+		sauceOpts.setCapability("tags", "w3c-chrome-tests");
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("sauce:options", sauceOpts);
+		cap.setCapability("browserVersion", versionName);
+		cap.setCapability("platformName", platformName);
+
+		if (browserName.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			cap.setCapability("browserName", "chrome");
+		} else if (browserName.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			cap.setCapability("browserName", "firefox");
+		}
+		// https://naveenanimation20:f418309b-b7b4-424d-825c-0d4a78b90bb5@ondemand.us-west-1.saucelabs.com:443/wd/hub
+		try {
+			driver = new RemoteWebDriver(new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub"), cap);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/com/mytests/SauceTest.java
@@ -0,0 +1,28 @@
+package com.mytests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SauceTest extends BaseTest {
+
+	public void doLogin() {
+		driver.get("https://www.saucedemo.com/index.html");
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+	}
+
+	@Test(priority = 1)
+	public void checkInventoryItemTest() {
+		doLogin();
+		Assert.assertTrue(driver.findElements(By.cssSelector(".inventory_item")).size() == 6);
+	}
+
+	@Test(priority = 2)
+	public void checkAddToCartButtonTest() {
+		doLogin();
+		Assert.assertTrue(driver.findElements(By.xpath("//button[text()='ADD TO CART']")).size() == 6);
+	}
+
+}
